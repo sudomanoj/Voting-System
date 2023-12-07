e3fd23554fa1bfee12e17c0b6134922daafa82e7
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2023-12-07 19:16:35 +0545

    added constituency.py file

---
+++ constituency.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class Election:
+    def __init__(self):
+        print('######Election#####')
+        
+    def election(self):
+        area = input('Enter Constituency: ')
+        date_of_election = input('Enter date ')
+        
